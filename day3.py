a770b59cc3c2736cbe3e31d0a4f907047e2f3de6
Chris Last <clast@qti.qualcomm.com>
2020-12-04 10:06:55 +0000

    day3

--- day3.py
+++ day3.py
@@ -2,12 +2,24 @@ from utils import *
 
 DAY = day(__file__)
 DATA = get_input(DAY)
-PARSED = [sscanf(_, r'(\d+)-(\d+) (.): (.*)', [int, int, str, str]) for _ in DATA]
+WIDTH = len(DATA[0])
+HEIGHT = len(DATA)
+
+def f1(right=3, down=1):
+    row = 0
+    col = 0
+    trees = 0
+    while row < HEIGHT:
+        if DATA[row][col] == "#":
+            trees += 1
+        row += down
+        col = (col + right) % WIDTH
+    return trees
 
 @part1
-def func():
-    pass
+def p1():
+    return f1()
 
 @part2
-def func():
-    pass
+def func2():
+    return f1(1,1) * f1(3,1) * f1(5,1) * f1(7,1) * f1(1,2)

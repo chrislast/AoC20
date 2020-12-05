e028dc87fda8eb396edc25ca83db038fef3baaab
Chris Last <chris.last@gmail.com>
2020-12-05 10:31:14 +0000

    day5

--- day5.py
+++ day5.py
@@ -2,12 +2,13 @@ from utils import *
 
 DAY = day(__file__)
 DATA = get_input(DAY)
-PARSED = [sscanf(_, r'(\d+)-(\d+) (.): (.*)', [int, int, str, str]) for _ in DATA]
+SEATS = [int(_.replace("F","0").replace("B","1").replace("L","0").replace("R","1"),2) for _ in DATA]
 
 @part1
-def func():
-    pass
+def func(expect=938):
+    return max(SEATS)
 
 @part2
-def func():
-    pass
+def func(expect=696):
+    all = set(range(min(SEATS),max(SEATS)+1))
+    return (all-set(SEATS)).pop()

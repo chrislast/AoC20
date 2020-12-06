1986a639ce0ad62899062804387cac5f82f08fad
Chris Last <clast@qti.qualcomm.com>
2020-12-06 10:38:00 +0000

    day6

--- day6.py
+++ day6.py
@@ -2,12 +2,34 @@ from utils import *
 
 DAY = day(__file__)
 DATA = get_input(DAY)
-PARSED = [sscanf(_, r'(\d+)-(\d+) (.): (.*)', [int, int, str, str]) for _ in DATA]
 
 @part1
-def func():
-    pass
+def func(expect=6726):
+    groups = []
+    groups.append(set())
+    for line in DATA:
+        if not line:
+            groups.append(set())
+        else:
+
+            for yes in line:
+                groups[-1].add(yes)
+    return sum(map(len, groups))
 
 @part2
-def func():
-    pass
+def func(expect=3316):
+    groups = []
+    l=0
+    groups.append(set())
+    for line in DATA:
+        if not line:
+            groups.append(set())
+            l=0
+        else:
+            answers = {_ for _ in line}
+            if l == 0:
+                groups[-1] = answers
+            else:
+                groups[-1] &= answers
+            l+=1
+    return sum(map(len, groups))

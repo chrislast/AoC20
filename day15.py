ab58c482590725d1c594b318db92966f97cc0593
Chris Last <chris.last@gmail.com>
2020-12-16 23:13:07 +0000

    day15 slow

--- day15.py
+++ day15.py
@@ -1,13 +1,59 @@
 from utils import *
 
 DAY = day(__file__)
-DATA = get_input(DAY)
-PARSED = [sscanf(_, r'(\d+)-(\d+) (.): (.*)', [int, int, str, str]) for _ in DATA]
+MEM = {
+20: [1, None],
+0:  [2, None],
+1:  [3, None],
+11: [4, None],
+6:  [5, None],
+3:  [6, None],
+}
 
 @part1
-def func():
-    pass
+def func(expect=421):
+    turn = len(MEM)+1
+    last = list(MEM.keys())[-1]
+    while turn <= 2020:
+        t1,t2 = MEM[last]
+        if t2 is None:
+            this = 0
+        else:
+            this = t1 - t2
+        if this in MEM:
+            MEM[this][1] = MEM[this][0]
+            MEM[this][0] = turn
+        else:
+            MEM.update({this:[turn, None]})
+        last = this
+        turn += 1
+    return last
+
+MEM = {
+20: [1, None],
+0:  [2, None],
+1:  [3, None],
+11: [4, None],
+6:  [5, None],
+3:  [6, None],
+}
 
 @part2
-def func():
-    pass
+def func(expect=436):
+    turn = len(MEM)+1
+    last = list(MEM.keys())[-1]
+    while turn <= 30000000:
+        t1,t2 = MEM[last]
+        if t2 is None:
+            this = 0
+        else:
+            this = t1 - t2
+        if this in MEM:
+            MEM[this][1] = MEM[this][0]
+            MEM[this][0] = turn
+        else:
+            MEM.update({this:[turn, None]})
+        last = this
+        turn += 1
+    return last
+

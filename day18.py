9d60531525adc87f7ab78e5d5957006ab118d81c
Chris Last <clast@qti.qualcomm.com>
2020-12-19 15:34:29 +0000

    day18 - note to self str.replace replaces ALL occurrences unless you add count param...

--- day18.py
+++ day18.py
@@ -2,12 +2,66 @@ from utils import *
 
 DAY = day(__file__)
 DATA = get_input(DAY)
-PARSED = [sscanf(_, r'(\d+)-(\d+) (.): (.*)', [int, int, str, str]) for _ in DATA]
+
+def e(t):
+    tokens = t.replace("(","").replace(")","").split()
+    acc = 0
+    op = "+"
+    for token in tokens:
+        if token in '+*':
+            op = token
+        elif op == "+":
+            acc += int(token)
+        elif op == "*":
+            acc *= int(token)
+    return str(acc)
 
 @part1
-def func():
-    pass
+def func(expect=75592527415659):
+    acc = 0
+    for t in DATA:
+        # Brackets Out
+        while True:
+            m = re.search(r"(\([^()]*\))", t)
+            if not m:
+                # evaluate sum
+                break
+
+            else:
+                # replace bracketed sum with evaluated bracket result
+                t = t.replace(m.group(1), e(m.group(1)), 1)
+                #print(t)
+        acc += int(e(t))
+    return acc
+
+def e2(t):
+    while True:
+        m = re.search(r"(\d+ \+ \d+)", t)
+        if not m:
+            break
+        else:
+            # replace plus sum with result
+            t = t.replace(m.group(1), e(m.group(1)), 1)
+            #print(t)
+    return e(t)
+
 
 @part2
-def func():
-    pass
+def func(expect=360029542265462):
+    acc = 0
+    for t in DATA:
+        #print(f"************\n{t}")
+        # Brackets Out
+        while True:
+            m = re.search(r"(\([^()]*\))", t)
+            if not m:
+                # evaluate sum
+                break
+            else:
+                # replace bracketed sum with evaluated bracket result
+                t = t.replace(m.group(1), e2(m.group(1)), 1)
+                #print(t)
+        ans = e2(t)
+        #print(ans)
+        acc += int(ans)
+    return acc
